c163bcae31dbba0de94b0e9fc075771900444b97
Aaron Foucheaux <afoucheaux@gmail.com>
2020-11-02 09:16:54 -0600

    working file

---
+++ test java.js
@@ -0,0 +1,13 @@
+var food = "Hot Pot";
+var is_delicious = true;
+
+food = "PB &J.";
+
+ var num2 = 10
+ var num7 = 20
+ var quotient = num7 / num2;
+
+ var songs = ["song1", "song2", "song3"];
+
+songs.pop();
+console.log(foods.length);
